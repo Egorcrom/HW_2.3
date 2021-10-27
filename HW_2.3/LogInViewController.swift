91570c14f19772138cab5c489330762ba55a6539
Egor Kromin <egorcrom@mail.ru>
2021-10-27 03:35:48 +0300

    basic front-end added

---
+++ HW_2.3/LogInViewController.swift
@@ -0,0 +1,33 @@
+//
+//  LogInViewController.swift
+//  HW_2.3
+//
+//  Created by Егор Кромин on 27.10.2021.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        CAGradientLayer()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func logOutButtonAction() {
+        dismiss(animated: true)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
